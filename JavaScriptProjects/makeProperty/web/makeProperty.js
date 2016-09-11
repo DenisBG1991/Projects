f7c874897e8487b70eb089e61d6d05d5c9a530af
Денис <bdg1991@mail.ru>
2016-09-11 23:35:14 +0300

    del

--- JavaScriptProjects/makeProperty/web/makeProperty.js
+++
@@ -1,9 +0,0 @@
-function makeProperty(o, name, predicate){
-    var value;
-    o["get"+name] = function (){return value};
-    o["set"+name] = function (v){
-        if (predicate && !predicate(v)){
-            throw new Error ("set" + name + " неверное значение : " + v);}
-        else value = v;
-    }
-}
