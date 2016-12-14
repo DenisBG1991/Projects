c3a98c8d22a3d21a898be50e47cf869b33471aa9
Денис <bdg1991@mail.ru>
2016-12-14 23:32:19 +0300

    usefulFeatures1.2

--- JavaScriptProjects/usefulFeatures/js/usefulFeatures.js
+++ JavaScriptProjects/usefulFeatures/js/usefulFeatures.js
@@ -97,6 +97,36 @@ export function usefulFunctions() {
     }
 
     /**
+     * Возвращает тип значения в виде строки.
+     */
+    function type(o) {
+        var t, c, n;
+        if (o === null) return "null";
+        if (o !== o) return "NaN";
+        if ((t = typeof o) !== "object") return t;
+        if ((c = classof(o)) !== "Object") return c;
+        if (o.constructor && typeof o.constructor === "function" &&
+            (n = o.constructor.getName(o))) return n;
+        return "Object";
+    }
+
+    /**
+     * Возвращает класс объекта.
+     */
+    function classof(o) {
+        return Object.prototype.toString.call(o).slice(8, -1);
+    }
+
+    /**
+     * Возвращает имя функции (может быть "") или null - для объектов,
+     * не являющихся функциями.
+     */
+    Function.prototype.getName = function () {
+        if ("name" in this) return this.name;
+        return this.name = this.toString().match(/function\s*([^(]*)\(/)[1];
+    };
+
+    /**
      * Добавляет в прототип объекта Object новый метод extend, который копирует все
      * собственные перечислимые и неперечислимые свойства из объекта o в объект вызова.
      */
