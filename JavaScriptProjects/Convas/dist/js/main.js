0c9ef8f46350276920913b131fcc50256e233cc8
Денис <bdg1991@mail.ru>
2017-03-07 04:55:05 +0300

    add game

---
+++ JavaScriptProjects/Convas/dist/js/main.js
@@ -0,0 +1,164 @@
+"use strict";
+
+$(document).ready(function () {
+
+    var Myclock = function Myclock() {
+
+        var clock = void 0,
+            ctx = void 0,
+            cX = void 0,
+            cY = void 0,
+            R = void 0;
+        var buffer = null;
+        var now = new Date();
+        var time = {
+            ms: now.getMilliseconds(),
+            s: now.getSeconds(),
+            m: now.getMinutes(),
+            h: now.getHours()
+        };
+        var position = {
+            sec: {
+                x: null,
+                y: null
+            },
+            min: {
+                x: null,
+                y: null
+            },
+            hrs: {
+                x: null,
+                y: null
+            }
+        };
+        var angleS = void 0,
+            angleM = void 0,
+            angleH = void 0;
+
+        function drawClockBody() {
+            ctx.save();
+            ctx.beginPath();
+            ctx.lineWidth = "12";
+            ctx.strokeStyle = "#622D03";
+            ctx.arc(cX, cY, R, 0, Math.PI * 2);
+            ctx.fill();
+            ctx.stroke();
+            ctx.restore();
+        }
+
+        function drawClockFace() {
+            ctx.save();
+            ctx.lineWidth = 6;
+            ctx.strokeStyle = 'gray';
+            ctx.fillStyle = "#333";
+            for (var i = 1; i < 61; i++) {
+                ctx.beginPath();
+                var angle = i * Math.PI / 30 - Math.PI / 2;
+                var dx = Math.cos(angle) * R;
+                var dy = Math.sin(angle) * R;
+                if (i % 15 == 0) {
+                    ctx.font = '19px Tahoma';
+                    ctx.textAlign = 'center';
+                    ctx.textBaseline = "middle";
+                    ctx.fillStyle = "#A9B7C6";
+                    ctx.fillText(i / 5, cX + dx * 0.9, cY + dy * 0.9);
+                } else if (i % 5 == 0) {
+                    ctx.lineWidth = 1;
+                    ctx.arc(cX + 0.9 * dx, cY + 0.9 * dy, 1.5, 360, 0);
+                    ctx.fill();
+                } else {
+                    ctx.lineWidth = 1;
+                    ctx.arc(cX + 0.9 * dx, cY + 0.9 * dy, 0.5, 360, 0);
+                    ctx.fill();
+                }
+                ctx.stroke();
+            }
+            ctx.restore();
+        }
+
+        function drawSeconds() {
+            ctx.save();
+            angleS = (time.s + time.ms * 0.001) * Math.PI / 30 - Math.PI / 2;
+            position.sec.x = cX + Math.cos(angleS) * R * 0.88;
+            position.sec.y = cY + Math.sin(angleS) * R * 0.88;
+            ctx.beginPath();
+            ctx.strokeStyle = "#DB1E00";
+            ctx.moveTo(cX, cY);
+            ctx.lineTo(position.sec.x, position.sec.y);
+            ctx.stroke();
+            ctx.restore();
+        }
+
+        function drawMinutes() {
+            ctx.save();
+            angleM = (time.m + time.s / 60) * Math.PI / 30 - Math.PI / 2;
+            position.min.x = cX + Math.cos(angleM) * R * 0.66;
+            position.min.y = cY + Math.sin(angleM) * R * 0.66;
+            ctx.beginPath();
+            ctx.lineWidth = 4;
+            ctx.strokeStyle = "#BC902D";
+            ctx.lineCap = 'round';
+            ctx.moveTo(cX, cY);
+            ctx.lineTo(position.min.x, position.min.y);
+            ctx.stroke();
+            ctx.restore();
+        }
+
+        function drawHours() {
+            ctx.save();
+            angleH = (time.h + time.m / 60) * Math.PI / 6 - Math.PI / 2;
+            position.hrs.x = cX + Math.cos(angleH) * R * 0.33;
+            position.hrs.y = cY + Math.sin(angleH) * R * 0.33;
+            ctx.beginPath();
+            ctx.lineWidth = 9;
+            ctx.strokeStyle = "#F18F08";
+            ctx.lineCap = 'round';
+            ctx.moveTo(cX, cY);
+            ctx.lineTo(position.hrs.x, position.hrs.y);
+            ctx.stroke();
+            ctx.restore();
+        }
+
+        function init(canvasId) {
+            if (canvasId && canvasId.length > 0) {
+                clock = document.getElementById(canvasId);
+                if (clock === null) {
+                    console.error('Myclock init error: элемент canvas с id = "' + canvasId + '" не найден');
+                    return -1;
+                }
+            } else {
+                console.error('Myclock init error: не задан идентификатор холста');
+                return -1;
+            }
+            clock.width = 400;
+            clock.height = 400;
+            ctx = clock.getContext('2d');
+            cX = clock.offsetLeft + clock.width / 2;
+            cY = clock.offsetTop + clock.height / 2;
+            R = 150;
+
+            drawClockBody();
+            drawClockFace();
+            buffer = ctx.getImageData(0, 0, clock.width, clock.height);
+            setInterval(update, 10);
+        }
+
+        function update() {
+            now = new Date();
+            time.ms = now.getMilliseconds();
+            time.h = now.getHours();
+            time.m = now.getMinutes();
+            time.s = now.getSeconds();
+            ctx.putImageData(buffer, 0, 0);
+            drawSeconds();
+            drawMinutes();
+            drawHours();
+        }
+
+        this.init = init;
+    };
+
+    var myclock = new Myclock();
+    myclock.init("clock");
+});
+//# sourceMappingURL=main.js.map
