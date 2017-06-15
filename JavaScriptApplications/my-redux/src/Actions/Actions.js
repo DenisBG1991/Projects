a0f5cdd8108785963b3dabfecff999f3e9fd964d
Денис <bdg1991@mail.ru>
2017-06-16 01:05:59 +0300

    redux

--- JavaScriptApplications/my-redux/src/Actions/Actions.js
+++ JavaScriptApplications/my-redux/src/Actions/Actions.js
@@ -1,13 +1,45 @@
+// function incrementAction(amount) {
+//     return { type: 'INCREMENT', amount };
+// }
+//
+// function decrementAction(amount) {
+//     return { type: 'DECREMENT', amount };
+// }
+//
+// function resetAction() {
+//     return { type: 'RESET' };
+// }
+
 function incrementAction(amount) {
-    return { type: 'INCREMENT', amount };
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve({
+                type: 'INCREMENT',
+                amount
+            });
+        }, 1000);
+    });
 }
 
 function decrementAction(amount) {
-    return { type: 'DECREMENT', amount };
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve({
+                type: 'DECREMENT',
+                amount
+            });
+        }, 1000);
+    });
 }
 
 function resetAction() {
-    return { type: 'RESET' };
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve({
+                type: 'RESET'
+            });
+        }, 1000);
+    });
 }
 
 export {incrementAction, decrementAction, resetAction};
--- JavaScriptApplications/my-redux/src/Counter/Counter.js
+++ JavaScriptApplications/my-redux/src/Counter/Counter.js
@@ -51,14 +51,83 @@ export default Counter;
 
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-//import { createStore } from 'redux';                  // This is the implementation of the developers facebook
-import createStore from '../CreateStore/CreateStore';   // This is my the implementation of createStore
+import { createStore } from 'redux';                        // This is the implementation of the developers facebook
+//import createStore from '../CreateStore/CreateStore';     // This is my the implementation of createStore
 import reducer from '../Reducer/Reducer';
 import * as actions from '../Actions/Actions';
 import './Counter.css';
 
 const initialState = { count: 0 };
+
+/**
+ * Примеры обработчиков для функции Middleware
+ **/
+/*
+const addLog = store => next => action => {
+    console.log('Состояние до действия:', store.getState());
+    console.log('Действия:', action.type, action);
+    const result = next(action);
+    console.log('Состояние после действия:', store.getState());
+    console.log(result);
+    return result;
+};
+
+const addDouble = store => next => action => {
+    action.amount *= 2;
+    return next(action);
+};
+
+const addTriple = store => next => action => {
+    action.amount *= 3;
+    return next(action);
+};
+*/
+
+/**
+ * Обычная реализация Middleware
+ **/
+/*
 const store = createStore(reducer, initialState);
+
+function applyMiddleware(store, ...middlewares) {
+    middlewares.reverse().forEach( middleware => store.dispatch = middleware(store)(store.dispatch));
+}
+
+applyMiddleware(store, addLog, addDouble, addTriple);
+
+const store = createStore(reducer, initialState, applyMiddleware(addLog, addDouble, addTriple));
+*/
+
+/**
+ * Реализация функции Middleware как у разработчиков Redux
+ **/
+function applyMiddleware(...middlewares) {
+    return createStore => (reducer, initialState, enhacer) => {
+        const store = createStore(reducer, initialState, enhacer);
+
+        let dispatch = store.dispatch;
+        let chain;
+
+        const storeApi = {
+            getState: store.getState,
+            dispatch: actions => dispatch(actions)
+        };
+
+        chain = middlewares.map(middleware => middleware(storeApi));
+        dispatch = chain.reduce((a, b) => (...args) => a(b(...args)))(store.dispatch);
+
+        return Object.assign(store, { dispatch });
+    };
+}
+
+const promise = store => next => actions => {
+    if (typeof actions.then === 'function'){
+        return actions.then(next);
+    }
+    return next(actions);
+};
+
+const store = createStore(reducer, initialState, applyMiddleware(promise));
 
 class Counter extends Component {
     constructor(props) {
@@ -70,7 +139,7 @@ class Counter extends Component {
 
     componentDidMount() {
         store.subscribe(() => this.forceUpdate());
-    }
+    };
 
     increment() {
         let amount = parseInt(ReactDOM.findDOMNode(this.refs.amount).value || 1, 10);
